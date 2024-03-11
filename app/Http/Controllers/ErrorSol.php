08241d87489bc97a41b2e7143bdea7d746eca4d0
Kundan2099 <121600019+Kundan2099@users.noreply.github.com>
2024-03-11 16:53:44 +0530

    sol

--- app/Http/Controllers/ErrorSol.php
+++ app/Http/Controllers/ErrorSol.php
@@ -1,10 +1,18 @@
 <?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ErrorSol extends Controller
+{
+    <?php
+
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+
 
 class JobPostController extends Controller 
 {
@@ -24,25 +32,21 @@ class JobPostController extends Controller
     // Method to store a newly created job post in the database
     public function store(Request $request)
     {
-        $validation = Validator::make($request->all(), [
-            'id' => ['required', 'string'],
-            'title' => ['required', 'string', 'min:1', 'max:250'],
-            'description' => ['required', 'string', 'min:1', 'max:250'],
-            'summary' => ['required', 'string', 'min:1', 'max:250'],
-            'skills' => ['required', 'string', 'min:1', 'max:250'],
-            'salary' => ['required', 'string', 'min:1', 'max:250'],
-            'location' => ['required', 'string'],
-            'emoloment_type' => ['required', 'string'],
-            'documents' => ['required', 'file', ],
-            'apply_url' => ['required', 'string'],
-            'status' => ['required', 'string'],
+        $request->validate([
+            'id' =>'required',
+            'title' => 'required',
+            'description' => 'required',
+            'summary' =>'required',
+            'skills' =>'required',
+            'salary' =>'required',
+            'location' =>'required',
+            'emoloment_type' =>'required',
+            'documents' =>'required',
+            'apply_url' =>'required',
+            'status' =>'required',
+            
+            // Add more validation rules as needed
         ]);
-
-        if ($validation->fails()) {
-            return redirect()->back()->withErrors($validation)->withInput();
-        }
-
-
 
         JobPost::create($request->all());
 
@@ -76,4 +80,5 @@ class JobPostController extends Controller
     }
 
     
+}
 }
